9431bcba289db87eafc3dd49893e8cd3a1a50f2b
Amar nath <amarnathu63@gmail.com>
2022-06-18 08:32:15 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void ansQ(){
+    print("answer choosed");
+  }
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('quiz!!!!'),
+          title: Text('quiz'),
         ),
         body: Column(children: <Widget>[
           Text("The quis!!"),
           ElevatedButton(
             child: Text('anw1'),
-             onPressed: null),
+             onPressed: ansQ),
           ElevatedButton(
             child: Text('anw2'), 
-            onPressed: null),
+            onPressed: ansQ),
           ElevatedButton(
             child: Text('anw3'), 
-            onPressed: null),
+            onPressed: ansQ),
         ]),
       ),
     );
